70e4c1364fcc067304bf5b9f2ba3b9c3a6337117
PrinceSonfack <princesonfackm@gmail.com>
2022-01-13 07:03:17 +0100

    To test if everything gives

---
+++ e_association_clone/src/notrepackage/mainClass.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package notrepackage;
+
+/**
+ *
+ * @author perso
+ */
+public class mainClass {
+
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub");
+    }
+    
+}
