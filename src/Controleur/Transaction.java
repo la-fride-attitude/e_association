de46294c9940f23715337c505023760bb42b6299
la-fride-attitude <frida.ongmakagne2@facsciences-uy1.cm>
2022-01-20 20:40:35 +0100

    Interface 1.0

---
+++ src/Controleur/Transaction.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controleur;
+ import java.util.Date;
+/**
+ *
+ * @author Frida
+ */
+public class Transaction {
+    private Date date;
+    private String type;
+    private double montant;
+    private Gestioncompte compte;
+    
+}
